cd49ccf326d6ed4301c2c379e209dcc143e9e194
shanu2409 <shanusingh2409@gmail.com>
2023-06-10 04:21:52 +0530

    done

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import colors from "colors";
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Database ${conn.connection.host} ${conn.connection.name}`
+        .bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
